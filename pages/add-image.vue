3fd0ed37bc0fdc55de6b6db94f5afd1d37bd2d19
Marek Chrast <chrast_m@zts.Borets.GC>
2020-09-02 12:29:01 +0200

    Add new media with wp rest api

---
+++ pages/add-image.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <input type="text" name="name" />
+    <input id="wpimage" name="image" type="file" />
+    <button type="button" @click="addImageAxios">Pridat obrazok Axios</button>
+    <button type="button" @click="addImageFetch">Pridat obrazok Fetch</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    async addImageAxios() {
+      const token = localStorage.getItem('auth._token.local')
+
+      const formData = new FormData()
+      const img = document.getElementById('wpimage')
+      formData.append('file', img.files[0])
+
+      this.$axios.setHeader('Authorization', token)
+
+      const response = await this.$axios.post('/media', formData)
+      console.log(response)
+    },
+    addImageFetch() {
+      const token = localStorage.getItem('auth._token.local')
+      const formData = new FormData()
+      const img = document.getElementById('wpimage')
+      formData.append('file', img.files[0])
+
+      fetch('http://192.168.152.16/intranet/wp-json/wp/v2/media', {
+        method: 'POST',
+        headers: {
+          Authorization: token
+        },
+        body: formData
+      })
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => {
+          console.log(data)
+        })
+    }
+  }
+}
+</script>
+
+<style></style>
