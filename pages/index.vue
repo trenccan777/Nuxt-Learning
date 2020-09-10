b76e3ec58fc1686d301be979bf6bd8fe18dfaff1
Marek Chrast <chrast_m@zts.Borets.GC>
2020-09-10 14:45:28 +0200

    Change index text for DO

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1>Toto je homepage na heroku</h1>
+    <h1>Toto je homepage na digital ocean</h1>
     <h2>Example heading <b-badge>New</b-badge></h2>
     <div v-if="$auth.loggedIn">
       Práve je prihlásený používateľ {{ $auth.user.name }}
