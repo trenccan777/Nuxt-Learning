fa5487d178fc5d68c704671635fc96c1fa74042d
Marek Chrást <m.chrast@gmail.com>
2020-09-14 19:14:46 +0200

    Dev new user registration

---
+++ pages/registracia-pouzivatela.vue
@@ -0,0 +1,50 @@
+<template>
+  <form id="login-form" @submit="userRegistration">
+    <label>Používateľské meno:</label>
+    <input v-model="registration.username" type="text" class="reg-username" />
+    <label>Email:</label>
+    <input v-model="registration.email" type="email" class="reg-email" />
+    <label>Heslo:</label>
+    <input
+      v-model="registration.password"
+      type="password"
+      class="reg-password"
+    />
+    <button type="submit">Registrovať</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      registration: {
+        username: '',
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async userRegistration(e) {
+      e.preventDefault()
+      const token = localStorage.getItem('auth._token.local')
+
+      const data = {
+        username: this.registration.username,
+        password: this.registration.password,
+        email: this.registration.email
+      }
+
+      this.$axios.setHeader('Content-Type', 'application/json')
+      this.$axios.setHeader('accept', 'application/json')
+      this.$axios.setHeader('Authorization', token)
+
+      const response = await this.$axios.post('/users', data)
+      return response
+    }
+  }
+}
+</script>
+
+<style></style>
